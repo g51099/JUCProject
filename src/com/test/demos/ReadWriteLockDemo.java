aff2a9682a5727c210181b57b72194e9ddca0df2
g51099 <g51099@126.com>
2019-04-23 20:04:12 +0800

    提交 Git Hello
    Signed-off-by: g51099 <g51099@126.com>

--- src/com/test/demos/ReadWriteLockDemo.java
+++ src/com/test/demos/ReadWriteLockDemo.java
@@ -15,6 +15,8 @@ public class ReadWriteLockDemo {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello Git");
+
         ReadWriteThread rwt = new ReadWriteThread();//资源类
 
         new Thread(()->{
