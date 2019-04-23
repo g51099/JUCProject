a3d6986ab75e07092d85825c1a4c071f07f89dd0
g51099 <g51099@126.com>
2019-04-23 20:08:32 +0800

    提交 Git Hello2
    Signed-off-by: g51099 <g51099@126.com>

---
+++ src/github/test/TestAddClassToGitHub.java
@@ -0,0 +1,11 @@
+package github.test;
+
+/**
+ * @author xiaoshan
+ * @create 2019-04-23 20:06
+ */
+public class TestAddClassToGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub AddClass");
+    }
+}
